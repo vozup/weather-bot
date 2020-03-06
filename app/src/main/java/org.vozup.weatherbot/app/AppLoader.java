612bb6afc2661b7f76865d900f4f7f2b604a4bee
vozup <vozupserega@gmail.com>
2020-03-06 14:26:53 +0200

    Basic structure

---
+++ app/src/main/java/org.vozup.weatherbot/app/AppLoader.java
@@ -0,0 +1,11 @@
+package org.vozup.weatherbot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLoader {
+    public static void main(String... args) {
+        SpringApplication.run(AppLoader.class, args);
+    }
+}
